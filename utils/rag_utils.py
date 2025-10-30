14b15eacb0f5f484f971a1f38c8a1f4f27d52880
sr442 <130687606+sr442@users.noreply.github.com>
2025-10-30 13:18:39 +0530

    Update rag_utils.py

---
+++ utils/rag_utils.py
@@ -0,0 +1,12 @@
+import PyPDF2
+
+def extract_text_from_pdf(file):
+    text = ""
+    reader = PyPDF2.PdfReader(file)
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+def chunk_text(text, chunk_size=500):
+    words = text.split()
+    return [" ".join(words[i:i + chunk_size]) for i in range(0, len(words), chunk_size)]
